f99dfad4644d3fe164c09a280170aeae9d9e35a8
Rafael Souza <rafaelrochasouza@outlook.com.br>
2019-08-10 18:37:31 -0300

    Finding the mode problem fixed, with sorting added.

--- Chapter3.cpp
+++ Chapter3.cpp
@@ -1,40 +1,45 @@
 #include "Chapter3.h"
 
+int compare_func (const void*, const void*);
+
 void Chapter3::finding_mode() {
 
     cout << "------------- Finding the mode problem.\n";
 
     const int ARRAY_SIZE = 12;
 
-    int survey_data[ARRAY_SIZE] = {4, 7, 7, 9, 9, 9, 8, 3, 3, 3, 3, 10};
+    int survey_data[ARRAY_SIZE] = {4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10};
     int most_frequent = 0;
     int highest_frequency = 0;
     int current_frequency = 0;
 
-    // FIX
+    qsort (survey_data, ARRAY_SIZE, sizeof (int), compare_func);
+
+    /*for (int i = 0; i < ARRAY_SIZE; i++) {
+        cout << survey_data[i] << " ";
+    }
+
+    cout << "\n";*/
+
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        cout << "I: " << i << " Current frequency: " << current_frequency << " Highest Frequency: " << highest_frequency << " Most Frequent: " << most_frequent << endl;
         current_frequency++;
         if (i == ARRAY_SIZE - 1 || survey_data[i] != survey_data[i + 1]) {
             if (current_frequency > highest_frequency) {
                 highest_frequency = current_frequency;
                 most_frequent = survey_data[i];
             }
+            current_frequency = 0;
         }
     }
 
-    //cout << "Array: " << survey_data << endl;
     cout << "Mode of the array: " << most_frequent << endl;
 
-    //qsort (survey_data, ARRAY_SIZE, sizeof (int), compare_func);
+
 
 }
 
-int Chapter3::compare_func (const void *void_a, const void *void_b) {
-
-        int *int_a = (int *)(void_b);
-        int *int_b = (int *)(void_b);
-        return *int_a - *int_b;
+int compare_func (const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
 }
 
 void Chapter3::insertion_sort (int *array, int ARRAY_SIZE) {
