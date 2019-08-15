e1f2927a97073afb3139efe3fce205ef0a67cdcf
Rafael Souza <rafaelrochasouza@outlook.com.br>
2019-08-15 16:44:29 -0300

    Finished chapter 3, exercises next

--- Chapter3.cpp
+++ Chapter3.cpp
@@ -100,3 +100,113 @@ void Chapter3::license_cost (int category, double gross_sales) {
 
     cout << "Cost of business category " << category << " with gross sales of " << gross_sales << " getting a license is " << cost;
 }
+
+void Chapter3::highest_grades() {
+
+    const int ARRAY_SIZE = 10;
+
+    student student_array[ARRAY_SIZE] = {
+        {87, 10001, "Fred"},
+        {28, 10002, "Tom"},
+        {100, 10003, "Alistair"},
+        {78, 10004, "Sasha"},
+        {84, 10005, "Erin"},
+        {98, 10006, "Belinda"},
+        {75, 10007, "Leslie"},
+        {70, 10008, "Candy"},
+        {81, 10009, "Aretha"},
+        {68, 10010, "Veronica"}
+    };
+
+    int highest_postion = 0;
+
+    for (int i = 1; i < ARRAY_SIZE; i++) {
+        if (student_array[i].grade > student_array[highest_postion].grade) {
+             highest_postion = i;
+        }
+    }
+
+    cout << "Highest grade in array: " << student_array[highest_postion].grade << "\nBelongs to: " << student_array[highest_postion].name << endl;
+
+}
+
+double array_average(int int_array[], int ARRAY_SIZE) {
+
+    double sum = 0;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        sum += int_array[i];
+    }
+    double average = sum / ARRAY_SIZE;
+    return average;
+}
+
+void Chapter3::highest_sales() {
+
+    const int NUM_AGENTS = 3;
+    const int NUM_MONTHS = 12;
+
+    int sales[NUM_AGENTS][NUM_MONTHS] = {
+        {1856, 498, 30924, 87478, 328, 2653, 387, 3754, 387587, 2873, 276, 32},
+        {5865, 5456, 3983, 6464, 9957, 4785, 3875, 3838, 4959, 1122, 7766, 2534},
+        {23, 55, 67, 99, 265, 376, 232, 223, 4546, 564, 4544, 3434}
+    };
+    int highest_sales = sales[0][0];
+
+    for (int agent = 0; agent < NUM_AGENTS; agent++) {
+        for (int month = 0; month < NUM_MONTHS; month++) {
+            if (sales[agent][month] > highest_sales) {
+                highest_sales = sales[agent][month];
+            }
+        }
+    }
+
+    cout << "Highest sales: " << highest_sales << endl;
+
+    double highest_average = array_average(sales[0], 12);
+    for (int agent = 1; agent < NUM_AGENTS; agent++) {
+        double agent_average = array_average(sales[agent], 12);
+        if (agent_average > highest_average)
+            highest_average = agent_average;
+    }
+
+    cout << "Highest average monthly sales: " << highest_average << endl;
+}
+
+void Chapter3::dynamic_survey() {
+
+    const int MAX_RESPONSE = 10;
+
+    vector<int> survey_data;
+    int survey_response;
+    int histogram[MAX_RESPONSE];
+    int vector_size = 0;
+    int most_frequent = 0;
+
+    survey_data.reserve(30); // not necessary, but good to prevent the vector to keep resizing as we add values to it
+
+    cout << "Enter the next survey response or -1 to end: ";
+    cin >> survey_response;
+    while (survey_response != -1) {
+        survey_data.push_back(survey_response);
+        cout << "Enter the next survey response or -1 to end: ";
+        cin >> survey_response;
+    }
+
+    vector_size = survey_data.size();
+    for (int i = 0; i < MAX_RESPONSE; i++) {
+        histogram[i] = 0;
+    }
+
+    for (int i = 0; i < vector_size; i++) {
+        histogram[survey_data[i] - 1]++;
+    }
+
+    for (int i = 1; i < MAX_RESPONSE; i++) {
+        if (histogram[i] > histogram[most_frequent])
+            most_frequent = i;
+    }
+    most_frequent++;
+
+    cout << "\nMost frequent answer: " << most_frequent << endl;
+
+}
--- Chapter3.h
+++ Chapter3.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,11 +8,19 @@ class Chapter3 {
 
 private:
     const int MAX_RESPONSE = 10;
+    struct student {
+        int grade;
+        int student_id;
+        string name;
+    };
 
 public:
     void finding_mode();
     void insertion_sort (int*, int);
     int* get_histogram (int*, int);
     void license_cost (int, double);
+    void highest_grades();
+    void highest_sales();
+    void dynamic_survey();
 };
 
